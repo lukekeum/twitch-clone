2c54e1dec9504127311a919df2b2d7e35688486b
lukekeum <lukekeum11@gmail.com>
2022-02-17 02:27:11 +0900

    feat: setup cors option

--- main-server/src/server.class.ts
+++ main-server/src/server.class.ts
@@ -1,22 +1,46 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
+import fastifyCors, { FastifyCorsOptions } from 'fastify-cors';
+
 import rootRoute from './routes';
+import { CustomError, ErrorType } from './utils/customError.class';
 
 export default class Server {
   private readonly app: FastifyInstance;
+  private corsOptions: FastifyCorsOptions;
 
   constructor(options?: FastifyServerOptions) {
     this.app = fastify(options);
 
+    this.corsSetup(process.env.CORS_WHITELISTS);
+
+    this.app.register(fastifyCors, this.corsOptions);
     this.app.register(rootRoute, { prefix: '/' });
   }
 
   async start(port: string | number) {
     try {
       await this.app.listen(port, '');
-      this.app.log.info(`Server listening on ${port}`);
     } catch (err) {
       this.app.log.error(err);
       process.exit(1);
     }
+  }
+
+  corsSetup(whitelists: string) {
+    const whiltelist = whitelists.split(',');
+
+    this.corsOptions = {
+      origin: (origin, cb) => {
+        if (whiltelist.indexOf(origin) !== -1) {
+          cb(null, true);
+        } else {
+          throw new CustomError({
+            type: ErrorType.FORBIDDEN,
+            message: 'Not allowed access by cors',
+          });
+        }
+      },
+      credentials: true,
+    };
   }
 }
--- main-server/types/config.d.ts
+++ main-server/types/config.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test';
+      CORS_WHITELISTS: string;
       PORT: string;
 
       // Typeorm Setting
