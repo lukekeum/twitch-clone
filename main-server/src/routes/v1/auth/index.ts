a9d1ad77791c544d238d814670d5c152e93b8a4f
lukekeum <lukekeum11@gmail.com>
2022-02-17 06:53:18 +0900

    feat: add json validation schema into auth route

--- main-server/src/routes/v1/auth/index.ts
+++ main-server/src/routes/v1/auth/index.ts
@@ -1,15 +1,19 @@
 import { FastifyPluginCallback, FastifyRequest } from 'fastify';
 import AuthService, { LoginBody, RegisterBody } from './authService';
+import LoginValidationJSON from './validation/login.validation.json';
+import RegisterValidationJSON from './validation/register.validation.json';
 
 const authRoute: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.post(
     '/login',
+    LoginValidationJSON,
     async (req: FastifyRequest<{ Body: LoginBody }>, res) => {
       return AuthService.login(req, res);
     }
   );
   fastify.post(
     '/register',
+    RegisterValidationJSON,
     async (req: FastifyRequest<{ Body: RegisterBody }>, res) => {
       return AuthService.register(req, res);
     }
