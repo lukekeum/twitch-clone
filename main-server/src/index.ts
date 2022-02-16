d6616774980b415805e59282d85bcf62b2c76977
lukekeum <lukekeum11@gmail.com>
2022-02-17 02:01:37 +0900

    feat: register root router

--- main-server/src/index.ts
+++ main-server/src/index.ts
@@ -1,4 +1,5 @@
 import './dotenv.config';
+import 'reflect-metadata';
 import Server from './server.class';
 import Database from './database.class';
 
--- main-server/src/server.class.ts
+++ main-server/src/server.class.ts
@@ -1,15 +1,19 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
+import rootRoute from './routes';
 
 export default class Server {
   private readonly app: FastifyInstance;
 
   constructor(options?: FastifyServerOptions) {
     this.app = fastify(options);
+
+    this.app.register(rootRoute, { prefix: '/' });
   }
 
   async start(port: string | number) {
     try {
       await this.app.listen(port, '');
+      this.app.log.info(`Server listening on ${port}`);
     } catch (err) {
       this.app.log.error(err);
       process.exit(1);
