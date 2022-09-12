3e6bd7247018838df674e3ae98e1353b572236b1
lukekeum <lukekeum11@gmail.com>
2022-09-12 19:26:41 +0900

    feat: add getUserByIdentifier query

--- main-server/src/graphql/user/user.resolver.ts
+++ main-server/src/graphql/user/user.resolver.ts
@@ -1,7 +1,7 @@
 import { User } from '@src/entities/user.entity';
 import { IsLoggedIn } from '@src/hooks/graphql/isLoggedIn';
 import { ContextType } from '@src/utils/graphql';
-import { Ctx, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { Arg, Ctx, Query, Resolver, UseMiddleware } from 'type-graphql';
 
 @Resolver()
 export class UserResolver {
@@ -9,6 +9,15 @@ export class UserResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req }: ContextType): Promise<User | undefined> {
     const user = await User.findOne(req.userId);
+
+    return user;
+  }
+
+  @Query(() => User, { nullable: true })
+  async getUserByIdentifier(
+    @Arg('identifier', () => String) identifier: string
+  ) {
+    const user = await User.findOne({ identifier });
 
     return user;
   }
