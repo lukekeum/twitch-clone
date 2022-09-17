0c1c46446ee2355fc86e83a9297dd6910e73da61
lukekeum <lukekeum11@gmail.com>
2022-09-17 14:37:24 +0900

    feat: add twitchIcon

---
+++ design/src/Icons/svg/icon/index.ts
@@ -0,0 +1,5 @@
+import { default as twitchFigure } from './TwitchFigure.svg';
+
+export default {
+  twitchFigure,
+};
--- design/src/Icons/svg/index.ts
+++ design/src/Icons/svg/index.ts
@@ -1,1 +1,19 @@
-export * as player from './player';
+import player from './player';
+import icon from './icon';
+import React, { SVGProps } from 'react';
+
+type FamilyNames = keyof typeof icons;
+type IconName<T extends FamilyNames> = keyof typeof icons[T];
+
+export type Props<T extends FamilyNames, N extends IconName<T>> = {
+  [key in T]: {
+    [family in N]: React.FunctionComponent<SVGProps<SVGElement>>;
+  };
+};
+
+const icons = {
+  player,
+  icon,
+};
+
+export default icons;
