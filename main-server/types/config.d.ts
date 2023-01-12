9f9cf497fec122f9265c37ae874dcdb14c1193c4
lukekeum <lukekeum11@gmail.com>
2023-01-13 00:24:44 +0900

    feat: add VERSION variable type

--- main-server/types/config.d.ts
+++ main-server/types/config.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test';
       CORS_WHITELISTS: string;
+      VERSION: string;
       PORT: string;
 
       ACCESS_JWT_SECRET: string;
