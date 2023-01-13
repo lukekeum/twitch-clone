8cdb35a293d8417dbe91972378304787da45e1d8
lukekeum <lukekeum11@gmail.com>
2023-01-13 18:34:13 +0900

    feat: apply type-graphql-dataloader plugin

--- main-server/src/utils/graphql/index.ts
+++ main-server/src/utils/graphql/index.ts
@@ -1,5 +1,7 @@
 import { Config } from 'apollo-server-fastify';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { ApolloServerLoaderPlugin } from 'type-graphql-dataloader';
+import { getConnection } from 'typeorm';
 import generateSchema from './schema';
 
 export default function getGraphqlServerOptions() {
@@ -12,6 +14,11 @@ export default function getGraphqlServerOptions() {
       res,
       cookies: { accessToken: req.cookies.access_token, qid: req.cookies.qid },
     }),
+    plugins: [
+      ApolloServerLoaderPlugin({
+        typeormGetConnection: getConnection,
+      }),
+    ],
   };
 
   return graphqlServerOption;
