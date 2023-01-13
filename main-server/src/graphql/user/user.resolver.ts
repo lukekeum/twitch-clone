d36e9a03909523d622f1840084f1abe4f916f96c
lukekeum <lukekeum11@gmail.com>
2023-01-13 18:35:41 +0900

    feat: add followers/followings field resolver

--- main-server/src/graphql/user/user.resolver.ts
+++ main-server/src/graphql/user/user.resolver.ts
@@ -1,9 +1,22 @@
+import DataLoader from 'dataloader';
+import { Follow } from '@src/entities/follow.entity';
 import { User } from '@src/entities/user.entity';
 import { IsLoggedIn } from '@src/hooks/graphql/isLoggedIn';
 import { ContextType } from '@src/utils/graphql';
-import { Arg, Ctx, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { groupBy } from 'lodash';
+import {
+  Arg,
+  Ctx,
+  FieldResolver,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware,
+} from 'type-graphql';
+import { Loader } from 'type-graphql-dataloader';
+import { In } from 'typeorm';
 
-@Resolver()
+@Resolver(() => User)
 export class UserResolver {
   @UseMiddleware([IsLoggedIn(true)])
   @Query(() => User, { nullable: true })
@@ -17,8 +30,38 @@ export class UserResolver {
   async getUserByIdentifier(
     @Arg('identifier', () => String) identifier: string
   ) {
-    const user = await User.findOne({ identifier });
+    const user = await User.createQueryBuilder('user')
+      .leftJoinAndSelect('user.userProfile', 'userProfile')
+      .leftJoinAndSelect('user.streamSetting', 'streamSetting')
+      .where('user.identifier = :id', { id: identifier })
+      .getOne();
 
     return user;
+  }
+
+  @FieldResolver(() => [Follow], { nullable: true })
+  @Loader<string, Follow[]>(async (ids) => {
+    const followers = await Follow.find({
+      where: { follower: { id: In([...ids]) } },
+    });
+    const followersById = groupBy(followers, 'userId');
+    return ids.map((id) => followersById[id] ?? []);
+  })
+  followers(@Root() user: User) {
+    return (dataloader: DataLoader<string, Follow[]>) =>
+      dataloader.load(user.id);
+  }
+
+  @FieldResolver(() => [Follow], { nullable: true })
+  @Loader<string, Follow[]>(async (ids) => {
+    const followings = await Follow.find({
+      where: { following: { id: In([...ids]) } },
+    });
+    const followingsById = groupBy(followings, 'userId');
+    return ids.map((id) => followingsById[id] ?? []);
+  })
+  followings(@Root() user: User) {
+    return (dataloader: DataLoader<string, Follow[]>) =>
+      dataloader.load(user.id);
   }
 }
