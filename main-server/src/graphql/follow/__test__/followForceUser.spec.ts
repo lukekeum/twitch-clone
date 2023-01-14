fc040094bdc2a0d008e428c0fcd3bba5221fd09f
lukekeum <lukekeum11@gmail.com>
2023-01-15 00:24:56 +0900

    test: add followForceUser/followUser test

--- main-server/src/graphql/follow/__test__/followForceUser.spec.ts
+++ main-server/src/graphql/follow/__test__/followForceUser.spec.ts
@@ -1,25 +1,20 @@
 import { Follow } from '@src/entities/follow.entity';
-import { User, UserAuthTokens } from '@src/entities/user.entity';
+import { User } from '@src/entities/user.entity';
 import generateUserObject from '@src/utils/generateUserObject';
 import TestUtils from '@src/utils/tests/utils.class';
 import { gql } from 'apollo-server-fastify';
 
-describe('MUTATE followUser', () => {
+describe('MUTATE followForceUser', () => {
   const user = generateUserObject();
   const target = generateUserObject();
   const req = {};
   const res = {};
 
   const MUTATION = gql`
-    mutation ($id: String!) {
-      followUser(targetId: $id)
+    mutation ($usrId: String!, $targetId: String!) {
+      followForceUser(userId: $usrId, targetId: $targetId)
     }
   `;
-
-  let tokens: UserAuthTokens = {
-    accessToken: '',
-    refreshToken: '',
-  };
 
   let usr: { user: User };
   let targetUsr: { user: User };
@@ -27,8 +22,6 @@ describe('MUTATE followUser', () => {
   beforeAll(async () => {
     usr = await TestUtils.addUser(user);
     targetUsr = await TestUtils.addUser(target);
-
-    tokens = await usr.user.generateTokens();
   });
 
   afterAll(async () => {
@@ -36,70 +29,42 @@ describe('MUTATE followUser', () => {
     await TestUtils.removeUserByEmail(target.email);
   });
 
-  it('Cookie not found', async () => {
-    const { mutate } = TestUtils.getGraphqlTestingTool({
-      context: () => ({
-        req,
-        res,
-        cookies: {
-          accessToken: '',
-          qid: '',
-        },
-      }),
-    });
-
-    const { errors } = await mutate({
-      mutation: MUTATION,
-      variables: { id: target.identifier },
-    });
-
-    expect(errors).toBeDefined();
-  });
-
   it('Target identifier is equal to user', async () => {
     const { mutate } = TestUtils.getGraphqlTestingTool({
       context: () => ({
         req,
         res,
-        cookies: {
-          accessToken: tokens.accessToken,
-          qid: tokens.refreshToken,
-        },
       }),
     });
 
     const { errors } = await mutate({
       mutation: MUTATION,
-      variables: { id: usr.user.identifier },
+      variables: { usrId: user.identifier, targetId: user.identifier },
     });
 
     expect(errors).toBeDefined();
   });
 
-  it('Valid cookie and input', async () => {
+  it('Follow force user succeed', async () => {
     const { mutate } = TestUtils.getGraphqlTestingTool({
       context: () => ({
         req,
         res,
-        cookies: {
-          accessToken: tokens.accessToken,
-          qid: tokens.refreshToken,
-        },
       }),
     });
 
     const { data, errors } = await mutate({
       mutation: MUTATION,
-      variables: { id: target.identifier },
+      variables: { usrId: user.identifier, targetId: target.identifier },
     });
 
-    expect(data.followUser).toBe(true);
+    expect(data.followForceUser).toBe(true);
     expect(errors).toBeUndefined();
 
     const follow = await Follow.findOne({
       where: {
-        follower: { id: usr.user.id },
-        following: { id: targetUsr.user.id },
+        follower: usr.user.id,
+        following: targetUsr.user.id,
       },
       relations: ['follower', 'following'],
     });
