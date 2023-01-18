68826b507be23b92917643b9f88dae37e641fe79
lukekeum <lukekeum11@gmail.com>
2023-01-18 16:08:19 +0900

    feat: add chat resolver

--- main-server/src/graphql/schema.ts
+++ main-server/src/graphql/schema.ts
@@ -4,12 +4,14 @@ import version from './version';
 import user from './user';
 import follow from './follow';
 import streamSetting from './streamSetting';
+import chat from './chat';
 
 const RESOLVERS: NonEmptyArray<Function> = [
   version,
   user,
   follow,
   streamSetting,
+  chat,
 ];
 
 export default RESOLVERS;
